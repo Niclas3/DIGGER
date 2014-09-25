2af2b1d2ae792ed37ae87d402d5bffe4d0438e16
niclas <missingurchin@gmail.com>
2014-09-26 00:30:29 +0800

    we can download image in each page.

--- digWebPage.py
+++ digWebPage.py
@@ -14,6 +14,16 @@ __author__ = 'Niclas'
 import urllib
 from bs4 import BeautifulSoup
 
+
+def typess(fn):
+    '''
+        This function to handle out question that
+        I want to know fn()'s return type
+    '''
+    def _warp():
+        return type(fn())
+    return _warp
+
 urlIndex = urllib.urlopen('http://www.yinwang.org/')
 context = urlIndex.read()
 soup = BeautifulSoup(context)
@@ -30,7 +40,7 @@ for i in range(len(articles)):
 
 # print downloadTable
 
-fb= open('./list.txt', 'a')  # appand list to the new list
+fb = open('./list.txt', 'a')  # appand list to the new list
 
 for url, title in downloadTable:
     fb.write('%s %s\n' % (url.encode('utf8'), title.encode('utf8')))
@@ -44,16 +54,15 @@ for articPair in downloadTable:  # To read each article's
     if(chrack == 'N'):
         continue
     else:
-        context = BeautifulSoup(artic.read())  # It is BeautifulSoup type not String
-        '''
+        context = BeautifulSoup(artic.read())
+        ''' It is BeautifulSoup type not String
             Ones page has img tag and I need download it
         '''
-        print context.find_all('img')
-        #with open('./'+title, 'w') as fbaitic:
-        #    fbaitic.write(str(context))
-
-
-
-
-
-
+        if context.find_all('img'):
+            for imgTag in context.find_all('img'):
+                with open('./'+imgName+'.jpeg', 'a') as img:
+                    img.write(urllib.urlopen(imgTag['src']).read())
+        else:
+            pass
+        # with open('./'+title, 'w') as fbaitic:
+        #     fbaitic.write(str(context))
